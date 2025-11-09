0fb12b21af41cd3f33dc01fc5fea8f9c01a9a565
Jagan S <rockjagan.822@gmail.com>
2025-11-09 12:23:27 +0530

    eslint config updated

--- eslint.config.js
+++ eslint.config.js
@@ -4,17 +4,21 @@ const js = require('@eslint/js');
 const reactNative = require('eslint-plugin-react-native');
 const prettier = require('eslint-plugin-prettier');
 const prettierConfig = require('eslint-config-prettier');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
 const compat = new FlatCompat();
 
 module.exports = [
     js.configs.recommended,
     ...compat.extends('airbnb'),
     ...compat.extends('airbnb/hooks'),
+    ...compat.extends('plugin:react-native/all'),
     prettierConfig,
     {
         files: ['**/*.{js,jsx,ts,tsx}'], // Apply to JavaScript and TypeScript files
         languageOptions: {
-            parser: require('@typescript-eslint/parser'),
+            parser: tsParser,
             ecmaVersion: 2021,
             sourceType: 'module',
             globals: {
@@ -24,7 +28,7 @@ module.exports = [
         plugins: {
             'react-native': reactNative,
             prettier,
-            '@typescript-eslint': require('@typescript-eslint/eslint-plugin'), // Added TypeScript plugin
+            '@typescript-eslint': tsPlugin, // Added TypeScript plugin
         },
         settings: {
             react: { version: 'detect' }, // Automatically detect the react version
@@ -51,8 +55,15 @@ module.exports = [
             'prettier/prettier': ['warn'], // Show prettier issues as warnings
             'import/no-extraneous-dependencies': [
                 'error',
-                { packageDir: ['./', 'node_modules/*'] },
+                { packageDir: ['./', 'node_modules/expo'] },
             ], // Allow importing devDependencies in certain files
+            'react-native/no-raw-text': 'off', // Allow raw text outside of Text component / custom components
+            'react-native/sort-styles': ['error', 'asc'], // Enable sorting of styles in ascending order
+            'react-native/no-color-literals': 'off', // Allow color literals in styles. disable temporarily. enable for better consistency
+            'react/require-default-props': 'off', // Disable requirement for default props in React components, since we are using function components. only for class components needed to avoid undefined
+            'react/jsx-props-no-spreading': 'off', // Allow props spreading in JSX
+            'import/prefer-default-export': 'off', // Allow single named exports
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Warn on unused variables, but ignore those starting with an underscore. Handy for interfaces with funcition parameters
         },
     },
 ];
